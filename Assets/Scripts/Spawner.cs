7fc634d42a2b3adc8285e656fb595a7a59c399c5
Andrey Polonskiy <fatcatdre@yandex.ru>
2023-10-06 11:23:46 -0400

    Implemented changes based on feedback
    Update variable names
    Change spawn logic from Update to a coroutine

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,39 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private Enemy _enemyPrefab;
-    [SerializeField] private float _timeBetweenSpawns = 2f;
+    [SerializeField] private Enemy _prefab;
+    [SerializeField] private float _delay = 2f;
 
     private SpawnPoint[] _spawnPoints;
-    private float _nextSpawnTime;
 
     private void Awake()
     {
         _spawnPoints = GetComponentsInChildren<SpawnPoint>();
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        TrySpawnEnemy();
+        StartCoroutine(SpawnEnemy());
     }
 
-    private void TrySpawnEnemy()
+    private void OnDisable()
     {
-        if (Time.time < _nextSpawnTime)
-            return;
-
-        SpawnEnemy();
-
-        _nextSpawnTime = Time.time + _timeBetweenSpawns;
+        StopCoroutine(SpawnEnemy());
     }
 
-    private void SpawnEnemy()
+    private IEnumerator SpawnEnemy()
     {
-        SpawnPoint spawnPoint = GetRandomSpawnPoint();
-        Enemy spawnedEnemy = Instantiate(_enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
+        while (enabled)
+        {
+            SpawnPoint spawnPoint = GetRandomSpawnPoint();
+            Enemy spawnedEnemy = Instantiate(_prefab, spawnPoint.transform.position, Quaternion.identity);
 
-        spawnedEnemy.transform.forward = spawnPoint.MoveDirection;
+            spawnedEnemy.transform.forward = spawnPoint.MoveDirection;
+
+            yield return new WaitForSeconds(_delay);
+        }
     }
 
     private SpawnPoint GetRandomSpawnPoint() => _spawnPoints[Random.Range(0, _spawnPoints.Length)];
